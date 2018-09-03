ad4f80868c31f625e02de30096e9415251048f58
Qua <qualab@gmail.com>
2018-09-03 10:13:12 +0300

    Test suite complete with checks for each template specialization.

--- tests/test_scalar.cpp
+++ tests/test_scalar.cpp
@@ -8,16 +8,32 @@ namespace dot
 {
     DOT_TEST_SUITE(scalar_creation)
     {
-        scalar<int> i = 123456789;
+        scalar<long long> LL = -1234567890123456789LL;
+        scalar<long> L = 123456789L;
+        scalar<int> i = -987654321;
+        scalar<short> s = short(-32100);
+        scalar<char> c = 'c';
+        scalar<unsigned long long> uLL = 12345678901234567890uLL;
+        scalar<unsigned long> uL = 4201234567uL;
+        scalar<unsigned int> ui = 0xbadfaced;
+        scalar<unsigned short> us = unsigned short(0x1eaf);
+        scalar<unsigned char> uc = 'u';
+        scalar<double> d = 987.65432109876;
+        scalar<float> f = 54.321f;
         scalar<bool> b = true;
-        scalar<long long> L = 1234567890123456789LL;
-        scalar<unsigned short> s = unsigned short(0x1eaf);
-        scalar<unsigned long> u = 4201234567uL;
-        DOT_CHECK(int(i)) == 123456789;
+        DOT_CHECK(long long(LL)) == -1234567890123456789LL;
+        DOT_CHECK(long(L)) == 123456789L;
+        DOT_CHECK(int(i)) == -987654321;
+        DOT_CHECK(short(s)) == -32100;
+        DOT_CHECK(char(c)) == 'c';
+        DOT_CHECK(unsigned long long(uLL)) == 12345678901234567890uLL;
+        DOT_CHECK(unsigned long(uL)) == 4201234567uL;
+        DOT_CHECK(unsigned int(ui)) == 0xbadfaced;
+        DOT_CHECK(unsigned short(us)) == 0x1eaf;
+        DOT_CHECK(unsigned char(uc)) == 'u';
+        DOT_CHECK(double(d)) == 987.65432109876;
+        DOT_CHECK(float(f)) == 54.321f;
         DOT_CHECK(bool(b)).is_true();
-        DOT_CHECK(long long(L)) == 1234567890123456789LL;
-        DOT_CHECK(unsigned short(s)) == unsigned short(0x1eaf);
-        DOT_CHECK(unsigned long(u)) == 4201234567uL;
     }
 }
 
