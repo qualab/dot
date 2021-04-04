4f5dbf6d960f2531a38b960218ac571506d6a72e
Vladimir Kerimov <v.kerimov@corp.mail.ru>
2021-04-04 16:01:40 +0300

    CMake projects generation has supported. Key press no more be awaited at the end of test_dot.

--- tests/test_dot.cpp
+++ tests/test_dot.cpp
@@ -1,14 +1,10 @@
 // DOT test main
 
 #include <dot/test.h>
-#include <iostream>
-#include <conio.h>
 
 int main()
 {
     dot::test::run();
-    std::cout << "\n >> Press any key ... " << std::flush;
-    getch();
     return 0;
 }
 
